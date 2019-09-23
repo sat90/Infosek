7e188a5c5c5d76c69c5aaf1a0ed1035e68054e00
GodNooNoo <emili.haava@gmail.com>
2019-09-23 16:08:05 +0200

    Update config.py
    changed ALLOWED_EXTENSIONS

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret' # TODO: Use this with wtforms
     DATABASE = 'database.db'
     UPLOAD_PATH = 'app/static/uploads'
-    ALLOWED_EXTENSIONS = {} # Might use this at some point, probably don't want people to upload any file type
+    ALLOWED_EXTENSIONS = {'bmp', 'gif', 'jpeg', 'jpg', 'tex',
+     'doc', 'docx', 'odt', 'txt', 'pdf', 'png', 'rtf'} # Only allowing some file types.
